1af66eb7c2d2e042c11b42ff4eff225f3ad2513f
dmcconi1 <david.l.mcconico@gmail.com>
2016-08-08 03:21:36 -0400

    Add files via upload

---
+++ Js/Assignment5.js
@@ -0,0 +1,47 @@
+function MenuChoice(selection)
+{
+document.getElementById("section1").style.visibility = "hidden";
+document.getElementById("section2").style.visibility = "hidden";
+document.getElementById("geolocation").style.visibility = "hidden";
+switch (selection)
+{
+case "section 1":
+document.getElementById("section1").style.visibility = "visible";
+break;
+case "section 2":
+document.getElementById("section2").style.visibility = "visible";
+break;
+case "geolocation":
+document.getElementById("geolocation").style.visibility = "visible";
+break;
+case "None":
+//No menu item selected, so no section should be displayed
+break;
+default:
+alert("Please select a different menu option");
+}
+}
+function Location() //Calls the Geolocation function built in to the web browser
+{
+var geo = navigator.geolocation; //References the Web Browser (navigator) geolocation service
+if (geo) //Tests to see if geolocation service is available
+{
+geo.getCurrentPosition(showPosition); //If the geolocation service is available it gets the position and calls a function to display it
+}
+else
+{
+alert("Geolocation is not supported"); //If the Geolocation service is not available it displays a message
+}
+}
+function showPosition(position) //Function receives the geolocation data and displays it
+{
+var latitude = position.coords.latitude; //Retrieves latitude data
+var longitude = position.coords.longitude; //Retrieves longitude data
+document.getElementById("latitude").innerHTML = latitude;
+document.getElementById("longitude").innerHTML = longitude;
+document.getElementById("map").style.visibility = "visible";
+document.getElementById("address").style.visibility = "visible";
+
+}
+
+
